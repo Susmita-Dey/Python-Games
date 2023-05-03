0299efb7df752bd185700ec454a655380faf0491
Susmita-Dey <susmitadey475@gmail.com>
2023-05-03 09:25:28 +0530

    Initialize with 2048 game

---
+++ 2048 Game/game.py
@@ -0,0 +1,25 @@
+# 2048 Game
+import tkinter as tk
+import random
+
+
+class Game(tk.Frame):
+    def __init__(self):
+        tk.Frame().__init__(self)
+        self.grid()
+        self.master.title("2048")
+        self.main_grid = tk.Frame(
+            self,
+            bg="#92877d",
+            bd=5,
+            width=800,
+            height=800
+        )
+
+
+def main():
+    print()
+
+
+if __name__ == "__main__":
+    main()
